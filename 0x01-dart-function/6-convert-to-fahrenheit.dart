f6ba78f227a57dd21740113671bab5286099ead5
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-11-30 18:41:17 -0500

    Add: convert temperature

---
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -0,0 +1,8 @@
+import 'dart:_js_helper';
+
+List<double> convertToF(List<double> temperaturesInC) {
+  List<double> temperaturesInF = temperaturesInC.map((temperatureInC) {
+    return double.parse((temperatureInC * 9 / 5 + 32).toStringAsFixed(2));
+  }).toList();
+  return temperaturesInF;
+}
