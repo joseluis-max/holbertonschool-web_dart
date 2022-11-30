3a8093e9dd29aa94d55c578aa7eed27de3384de7
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-11-30 18:25:50 -0500

    Closure

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,7 +1,7 @@
 void outer(String name, String id) {
   List<String> names  = name.split(" ");
   String inner() {
-  return "Hello Agent ${names[1]}.${names[1].substring(0,1)} your id is ${id}";
+  return "Hello Agent ${names[1].substring(0,1)}.${names[0]} your id is ${id}";
   }
   print(inner());
 }
