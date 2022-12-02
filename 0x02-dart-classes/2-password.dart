212afccdc8ae10c2bc76174efa238a10fe893941
Joseph Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-12-01 22:10:13 -0500

    Add: password

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,14 @@
+class Password {
+  String password = "";
+
+  bool isValid() {
+     return ((password.length >= 8) &&
+     (password.length <= 16)) &&
+        this.password.contains(new RegExp(r"[A-Z]"))
+  && this.password.contains(new RegExp(r"[a-z]"))
+  && this.password.contains(new RegExp(r"[0-9]"));
+  }
+
+  @override
+  String toString() => "Your Password is: ${this.password}";
+}
