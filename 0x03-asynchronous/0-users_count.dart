ffd96e88b63cf4f700a546f1bdd9bb7cc79bba56
José Luis Valdiviezo Peña <luisj.valdiviezo20@gmail.com>
2023-07-18 20:05:47 -0500

    Delete 0x03-asynchronous directory

--- 0x03-asynchronous/0-users_count.dart
+++
@@ -1,5 +0,0 @@
-import '0-util.dart';
-
-Future<void> usersCount() async{
-  print(await fetchUsersCount());  
-}
--- 0x03-asynchronous/1-get_user_id.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:convert';
-import '1-util.dart';
-
-Future<String> getUserId() {
-  return fetchUserData().then((user) => json.decode(user)['id']);
-}
